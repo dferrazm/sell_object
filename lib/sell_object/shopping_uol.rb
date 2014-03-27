5c8c3528655ab663ce9a63676b1830522105b0aa
Daniel Ferraz <d.ferrazm@gmail.com>
2014-03-27 17:51:52 -0300

    Add :to_shopping_uol class method

--- lib/sell_object/shopping_uol.rb
+++ lib/sell_object/shopping_uol.rb
@@ -1,20 +1,44 @@
 module SellObject
 	module ShoppingUol
 		XML_ELEMENT = %q{
-				<?xml version="1.0" encoding="iso-8859-1" ?>
-				<PRODUTOS>
 					<PRODUTO>
 						<CODIGO>:codigo</CODIGO>
 		  			<DESCRICAO>:descricao</DESCRICAO>
 		  			<PRECO>:preco</PRECO>
 		  			<URL>:url</URL>
 					</PRODUTO>
-				</PRODUTOS>
 		}
+
+		def self.included(base)
+	  	base.extend ClassMethods
+	  end
+
+	  module ClassMethods
+			# Class methods added on inclusion
+
+			def to_shopping_uol(objects)
+				elements = objects.map {|obj| obj.to_shopping_uol_element }.join ''
+				%Q{
+						<?xml version="1.0" encoding="iso-8859-1" ?>
+						<PRODUTOS>
+							#{elements}
+						</PRODUTOS>
+					}
+			end
+		end	 
 
 		# Instance methods added on inclusion
 
 		def to_shopping_uol
+			%Q{
+					<?xml version="1.0" encoding="iso-8859-1" ?>
+					<PRODUTOS>
+						#{to_shopping_uol_element}
+					</PRODUTOS>
+				}	
+		end
+
+		def to_shopping_uol_element
 			begin				
 				custom_mappings_hash = eval "SellObject::#{self.class.name}Mappings.shopping_uol"
 			rescue
@@ -25,6 +49,6 @@ module SellObject
 			custom_mappings_hash.each { |key, value| result_xml.gsub! ":#{key}", send(value).to_s }
 			default_mappings_hash.each { |key, value| result_xml.gsub! ":#{key}", send(value).to_s }
 			result_xml
-		end		
+		end
 	end
 end
--- spec/sell_object/shopping_uol_spec.rb
+++ spec/sell_object/shopping_uol_spec.rb
@@ -5,58 +5,105 @@ class Product
 end
 
 describe SellObject::ShoppingUol do
-	describe '#to_shopping_uol' do
-		let(:some_product) { Product.new }
+	let(:lame_product) { Product.new }
+	let(:boring_product) { Product.new }
+	let(:products) { [lame_product, boring_product] }
 
-		before do
-			some_product.stub(:id).and_return 'PR1'
-			some_product.stub(:description).and_return 'Some lame product'
-			some_product.stub(:price).and_return 10.5
-			some_product.stub(:url).and_return 'http://example.com/default-product'
+	before do
+		lame_product.stub(:id).and_return 'PR1'
+		lame_product.stub(:description).and_return 'Some lame product'
+		lame_product.stub(:price).and_return 10.5
+		lame_product.stub(:url).and_return 'http://example.com/lame-product'
+
+		boring_product.stub(:id).and_return 'PR2'
+		boring_product.stub(:description).and_return 'Some boring product'
+		boring_product.stub(:price).and_return 7
+		boring_product.stub(:url).and_return 'http://example.com/boring-product'
+	end
+
+	context 'using default mappings' do
+		describe 'class methods' do
+			describe '#to_shopping_uol' do
+				it 'generates the XML accordingly with all the objects attributes' do
+					expect(remove_xml_noise Product.to_shopping_uol(products)).to eq remove_xml_noise %q{
+						<?xml version="1.0" encoding="iso-8859-1" ?>
+						<PRODUTOS>
+							<PRODUTO>
+							  <CODIGO>PR1</CODIGO>
+							  <DESCRICAO>Some lame product</DESCRICAO>
+							  <PRECO>10.5</PRECO>
+							  <URL>http://example.com/lame-product</URL>
+							</PRODUTO>
+							<PRODUTO>
+							  <CODIGO>PR2</CODIGO>
+							  <DESCRICAO>Some boring product</DESCRICAO>
+							  <PRECO>7</PRECO>
+							  <URL>http://example.com/boring-product</URL>
+							</PRODUTO>
+						</PRODUTOS>
+					}
+				end
+			end
 		end
 
-		context 'using the default mappings' do
+		describe 'instance methods' do
 			it 'generates the XML accordingly with the object attributes' do
-				expect(remove_xml_noise some_product.to_shopping_uol).to eq remove_xml_noise %q{
+				expect(remove_xml_noise lame_product.to_shopping_uol).to eq remove_xml_noise %q{
 					<?xml version="1.0" encoding="iso-8859-1" ?>
 					<PRODUTOS>
 						<PRODUTO>
 						  <CODIGO>PR1</CODIGO>
 						  <DESCRICAO>Some lame product</DESCRICAO>
 						  <PRECO>10.5</PRECO>
-						  <URL>http://example.com/default-product</URL>
+						  <URL>http://example.com/lame-product</URL>
 						</PRODUTO>
 					</PRODUTOS>
 				}
 			end
+		end	
+	end
+
+	context 'using custom mappings' do
+		before do
+			lame_product.stub(:custom_description).and_return 'My custom lame product description'
+			lame_product.stub(:custom_url).and_return 'http://example.com/custom-product'
+
+			module SellObject::ProductMappings
+				def self.shopping_uol
+					{ descricao: :custom_description,  url: :custom_url }
+				end
+			end	
 		end
 
-		context 'using custom mappings'	do						
-			before do
-				some_product.stub(:custom_description).and_return 'My custom lame product description'
-				some_product.stub(:custom_url).and_return 'http://example.com/custom-product'
+		describe 'class methods' do
+			describe '#to_shopping_uol' do
+				before do
+					boring_product.stub(:custom_description).and_return 'My custom boring product description'
+					boring_product.stub(:custom_url).and_return 'http://example.com/boring-product'
+				end
 
-				module SellObject::ProductMappings
-					def self.shopping_uol
-						{ descricao: :custom_description,  url: :custom_url }
-					end
+				it 'generates the XML accordingly with all the objects attributes and the custom mappings' do
 				end
 			end
+		end
 
-			it 'generates the XML accordingly with the object attributes defined in the custom mappings' do
-				expect(remove_xml_noise some_product.to_shopping_uol).to eq remove_xml_noise %q{
-					<?xml version="1.0" encoding="iso-8859-1" ?>
-					<PRODUTOS>
-						<PRODUTO>
-						  <CODIGO>PR1</CODIGO>
-						  <DESCRICAO>My custom lame product description</DESCRICAO>
-						  <PRECO>10.5</PRECO>
-						  <URL>http://example.com/custom-product</URL>
-						</PRODUTO>
-					</PRODUTOS>
-				} 	
+		describe 'instance methods' do
+			describe '#to_shopping_uol' do
+				it 'generates the XML accordingly with the object attributes defined in the custom mappings' do
+					expect(remove_xml_noise lame_product.to_shopping_uol).to eq remove_xml_noise %q{
+						<?xml version="1.0" encoding="iso-8859-1" ?>
+						<PRODUTOS>
+							<PRODUTO>
+							  <CODIGO>PR1</CODIGO>
+							  <DESCRICAO>My custom lame product description</DESCRICAO>
+							  <PRECO>10.5</PRECO>
+							  <URL>http://example.com/custom-product</URL>
+							</PRODUTO>
+						</PRODUTOS>
+					} 	
+				end
 			end
-		end	
+		end
 	end
 end
 
